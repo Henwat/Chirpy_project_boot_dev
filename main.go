523a0e1d3c245e5f4772ef7a77fb327d1b3ca2e7
henwat <wathneh@gmail.com>
2024-10-29 19:42:59 +0100

    modified the handlers w/specifying methods

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
 func readinessHandler(w http.ResponseWriter, r *http.Request) {
@@ -11,14 +13,40 @@ func readinessHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("OK"))
 }
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+	hits := cfg.fileserverHits.Load()
+	fmt.Fprintf(w, "Hits: %d", hits)
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	fmt.Fprintf(w, "Hits reset to 0")
+}
+
 func main() {
 	const port = "8080"
+	apiCfg := &apiConfig{}
 
 	mux := http.NewServeMux()
-	fileServer := http.FileServer(http.Dir("."))
-	mux.Handle("/app/", http.StripPrefix("/app", fileServer))
+	fileServerHandler := http.FileServer(http.Dir("."))
+	strippedHandler := http.StripPrefix("/app", fileServerHandler)
+	finalHandler := apiCfg.middlewareMetricsInc(strippedHandler)
 
-	mux.HandleFunc("/healthz", readinessHandler)
+	mux.Handle("/app/", finalHandler)
+	mux.HandleFunc("GET /metrics", apiCfg.metricsHandler)
+	mux.HandleFunc("POST /reset", apiCfg.resetHandler)
+	mux.HandleFunc("GET /healthz", readinessHandler)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
